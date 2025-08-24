1c560cb52862d62b348bcbbc0ad874daea380dcd
C2H5CHO <2403692189@qq.com>
2025-08-24 20:51:30 +0800

    Update 07_张量的拼接操作.py

--- 【DL 第一弹】/DAY02：【DL 第一弹】pytorch/pytorch/07_张量的拼接操作.py
+++ 【DL 第一弹】/DAY02：【DL 第一弹】pytorch/pytorch/07_张量的拼接操作.py
@@ -1,2 +1,16 @@
 import torch
 
+torch.random.manual_seed(41)
+data1 = torch.randint(0, 10, [4, 5, 3])
+print(f"data1：{data1}")
+
+torch.random.manual_seed(42)
+data2 = torch.randint(0, 10, [4, 5, 5])
+print(f"data2：{data2}")
+
+print(f"dim=2：{torch.cat([data1, data2], dim=2)}")
+
+# 1×. 除拼接维度外，其余维度必须相同
+# print(f"dim=1：{torch.cat([data1, data2], dim=1)}")
+# # RuntimeError: Sizes of tensors must match except in dimension 1. Expected size 3 but got size 5 for tensor number 1 in the list.
+
