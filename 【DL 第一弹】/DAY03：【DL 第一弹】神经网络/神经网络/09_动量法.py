c13199fb1d761296515dae2035105fcad095f820
C2H5CHO <2403692189@qq.com>
2025-08-27 15:17:47 +0800

    Update 09_动量法.py

--- 【DL 第一弹】/DAY03：【DL 第一弹】神经网络/神经网络/09_动量法.py
+++ 【DL 第一弹】/DAY03：【DL 第一弹】神经网络/神经网络/09_动量法.py
@@ -1,3 +1,42 @@
 import torch
 import torch.nn as nn
 
+# （1）原始
+w = torch.tensor([1.0], requires_grad=True, dtype=torch.float32)
+loss = ((w**2) * 0.5).sum()
+
+optimizer = torch.optim.SGD([w], lr=0.01)
+
+optimizer.zero_grad()
+loss.backward()
+optimizer.step()
+print(w.grad)
+print(w.detach())
+
+loss = ((w**2) * 0.5).sum()
+optimizer.zero_grad()
+loss.backward()
+optimizer.step()
+print(w.grad)
+print(w.detach())
+
+print('--'*50)
+# （2）动量法
+w = torch.tensor([1.0], requires_grad=True, dtype=torch.float32)
+loss = ((w**2) * 0.5).sum()
+
+optimizer = torch.optim.SGD([w], lr=0.01, momentum=0.9)
+
+optimizer.zero_grad()
+loss.backward()
+optimizer.step()
+print(w.grad)
+print(w.detach())
+
+loss = ((w**2) * 0.5).sum()
+optimizer.zero_grad()
+loss.backward()
+optimizer.step()
+print(w.grad)
+print(w.detach())
+
