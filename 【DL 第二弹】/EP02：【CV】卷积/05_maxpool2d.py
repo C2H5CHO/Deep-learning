758e0755a926bb22b957155584e3a48966a53650
C2H5CHO <2403692189@qq.com>
2025-11-13 12:18:38 +0800

    add 05_maxpool2d.py

---
+++ 【DL 第二弹】/EP02：【CV】卷积/05_maxpool2d.py
@@ -0,0 +1,38 @@
+import torch
+from torch import nn
+
+# 1. 输入数据
+data = torch.ones(size=(10, 3, 28, 28))
+
+# 2. 卷积层
+conv1 = nn.Conv2d(
+    in_channels=3,
+    out_channels=6,
+    kernel_size=3
+)
+data_conv1 = conv1(data)
+print(f"第一层卷积层的形状: {data_conv1.shape}")
+conv2 = nn.Conv2d(
+    in_channels=6,
+    out_channels=16,
+    kernel_size=3,
+    stride=2,
+    padding=1
+)
+data_conv2 = conv2(data_conv1)
+print(f"第二层卷积层的形状: {data_conv2.shape}")
+
+# 3. 池化层
+pool1 = nn.MaxPool2d(
+    kernel_size=2 # 池化核大小，表示2x2的池化窗口
+)
+data_pool1 = pool1(data_conv2)
+print(f"第一层池化层的形状: {data_pool1.shape}")
+
+max_pool1 = nn.MaxPool2d(2) # 表示输出特征图的大小为7x7
+data_max_pool1 = max_pool1(data_conv2)
+print(f"第一层最大池化层的形状: {data_max_pool1.shape}")
+
+avg_pool1 = nn.AvgPool2d(2) # 表示输出特征图的大小为7x7
+data_avg_pool1 = avg_pool1(data_conv2)
+print(f"第一层平均池化层的形状: {data_avg_pool1.shape}")
