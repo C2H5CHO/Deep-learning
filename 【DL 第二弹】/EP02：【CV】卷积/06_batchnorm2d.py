4e4f75ce536c08df8619f07824b6fcf2c5af4216
C2H5CHO <2403692189@qq.com>
2025-11-13 16:24:20 +0800

    Update 06_batchnorm2d.py

--- 【DL 第二弹】/EP02：【CV】卷积/06_batchnorm2d.py
+++ 【DL 第二弹】/EP02：【CV】卷积/06_batchnorm2d.py
@@ -1,3 +1,20 @@
 import torch
 from torch import nn
 
+# 1. 输入数据
+data = torch.ones(size=(10, 3, 28, 28))
+
+# 2. 卷积层
+conv1 = nn.Conv2d(
+    in_channels=3,
+    out_channels=32,
+    kernel_size=5,
+    padding=2
+)
+data_conv1 = conv1(data)
+print(f"第一层卷积层的形状: {data_conv1.shape}")
+
+# 3. BN层
+bn1 = nn.BatchNorm2d(32) # 表示对32个特征图进行归一化
+data_bn1 = bn1(data_conv1)
+print(f"第一层BN层的形状: {data_bn1.shape}")
